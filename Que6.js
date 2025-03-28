b4c13efa0747d808fef7068b3bb8e90cfebdd243
Sanjana23435 <karemoresanju@gmail.com>
2025-03-28 08:52:22 +0530

    Assignment-8(js)
    JS ASSIGNMENT-8

--- Que6.js
+++ Que6.js
@@ -1,14 +1,6 @@
-function removeltem(arr, index){
-    const newArray = [];
-    for (let i=0;i<arr.length;i++) {
-        if (i==index) {
-            continue;
-        }
-        newArray.push(arr[i]);
-    }
-    return newArray;
+function startTimer() {
+    setTimeout(() => {
+        console.log("Time out");
+    }, 5000); 
 }
-const originalArray = [1,2,3,4,5];
-const indexToRemove = 2;
-const result = removeltem(originalArray,indexToRemove);
-console.log(result); 
+startTimer();
--- script.js
+++ script.js
@@ -1,9 +1,1 @@
-let para1 = document.querySelector('#yo1');
-let para2 = document.querySelector('#yo2');
-let para3 = document.querySelector('#yo3');
-let para4 = document.querySelector('#yo4');
-console.log(para1);
-console.log(para2);
-console.log(para3);
-console.log(para4);
-
+document.getElementById('fourth-paragraph').textContent = 'Fourth Paragraph';
